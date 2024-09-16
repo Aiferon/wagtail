ede611e113dc1b300255c310532740f68544382c
Aiferon <aiferon12@gmail.com>
2024-09-16 17:38:17 +0300

    Initial Wagtail project

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&yv71h#x^k-4dq(b-7f1gcpb*r)37=7(tqpn9fh%nf7qzd_zhm"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
