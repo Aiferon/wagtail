eedd932cfb4ea00acdef348eefb7f43e69515ab8
Aiferon <aiferon12@gmail.com>
2024-09-17 19:00:35 +0300

    Описание изменений

--- home/models.py
+++ home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
-    pass
+    
+    #Поля в базе данных   
+    subtitle = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Подзаголовок"
+    )
+
+    #Поля для ввода данных в интерфейсе
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle')
+    ]
